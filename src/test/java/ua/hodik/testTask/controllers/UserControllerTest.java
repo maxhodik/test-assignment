b85329d947c578de4f8ccf14995db68f0b3bb2d5
m-god <m-godik@ukr.net>
2024-04-24 23:29:00 +0200

    add tests
    (cherry picked from commit b5573433cd9d554f8f7cd65e6c7c07ee523232be)

--- src/test/java/ua/hodik/testTask/controllers/UserControllerTest.java
+++ src/test/java/ua/hodik/testTask/controllers/UserControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
